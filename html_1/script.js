8799864b547ef77addcce068e56a78ad05b06728
HS07 <hs07@Sa04s-MacBook-Air.local>
2026-03-02 17:51:26 -0500

    added work

---
+++ html_1/script.js
@@ -0,0 +1,17 @@
+let isloggedin = ture;
+let level = 5 
+if(isloggedin && level>=5){
+    {console.log} ("user can play level 5 or more");
+}
+
+else{
+    console.log ("the user is logged in");
+}
+
+let ispremiumuser = true;
+let coins = 10000;
+
+if (ispremiumuser || coin>10000){console.log ("user can play premimum levels");
+
+}
+else{is}
