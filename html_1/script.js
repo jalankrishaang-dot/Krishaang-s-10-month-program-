b6a053227eace645f47483945af44b9e11420ced
HS07 <hs07@Sa04s-MacBook-Air.local>
2026-03-04 18:11:25 -0500

    i improved my project and added new alerts to my javascript

--- html_1/script.js
+++ html_1/script.js
@@ -1,17 +1,31 @@
-let isloggedin = ture;
-let level = 5 
-if(isloggedin && level>=5){
-    {console.log} ("user can play level 5 or more");
-}
+alert('Welcome to shotify user');
 
-else{
-    console.log ("the user is logged in");
-}
+let age = 13;
+const birthyear = 2012;
 
-let ispremiumuser = true;
-let coins = 10000;
+console.log("my age is ", age);
 
-if (ispremiumuser || coin>10000){console.log ("user can play premimum levels");
+window.onload = function() {
+  
+    let userChoice = confirm("🍪 Do you allow targeted cookies?");
 
-}
-else{is}
+    if (userChoice) {
+        
+        alert("Welcome! You may enter the website.");
+       
+        
+    } else {
+       
+        alert("Sorry, you cannot log in to the website because you did not agree to the cookies.");
+        
+       
+        document.body.innerHTML = "";
+        document.body.style.backgroundColor = "black";
+
+
+        setTimeout(function () {
+            alert("sorry reload the page to try again (error 404).");
+        }, 100);
+    }
+};
+
